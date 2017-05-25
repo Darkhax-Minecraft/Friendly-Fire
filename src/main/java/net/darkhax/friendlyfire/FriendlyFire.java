bdbb22025d54eb4a84762272f4c46ed284a3fbfe
Tyler Hancock <darklime@live.ca>
2017-05-25 00:46:19 -0600

    Ported to 1.11.2

--- src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
+++ src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
@@ -11,34 +11,54 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = "friendlyfire", name = "Friendly Fire", version = "1.3.0", acceptableRemoteVersions = "*")
+@Mod(modid = "friendlyfire", name = "Friendly Fire", version = "@VERSION@", acceptableRemoteVersions = "*")
 public class FriendlyFire {
-    
+
     @EventHandler
     public void init (FMLPreInitializationEvent event) {
-        
+
         MinecraftForge.EVENT_BUS.register(this);
         new ConfigHandler(event.getSuggestedConfigurationFile());
     }
-    
+
     @SubscribeEvent
     public void onMobHit (LivingHurtEvent event) {
-        
+
         if (event.getEntityLiving() != null && event.getSource() != null && event.getSource().getEntity() != null) {
-            
+
             final EntityLivingBase living = event.getEntityLiving();
             final Entity source = event.getSource().getEntity();
-            
-            if (ConfigHandler.protectPets && living instanceof IEntityOwnable) {
-                
-                final Entity owner = ((IEntityOwnable) living).getOwner();
-                
-                if (owner != null && owner.getUniqueID().equals(source.getUniqueID()) && !source.isSneaking())
-                    event.setCanceled(true);
+
+            if (living instanceof IEntityOwnable) {
+
+                if (ConfigHandler.protectPets) {
+
+                    final Entity owner = ((IEntityOwnable) living).getOwner();
+
+                    if (owner != null && owner.getUniqueID().equals(source.getUniqueID()) && !source.isSneaking()) {
+
+                        event.setCanceled(true);
+                        return;
+                    }
+                }
+
+                else if (ConfigHandler.protectPetsFromPets && source instanceof IEntityOwnable) {
+
+                    final boolean sameOwner = ((IEntityOwnable) living).getOwnerId().equals(((IEntityOwnable) source).getOwnerId());
+
+                    if (sameOwner) {
+
+                        living.setRevengeTarget(null);
+                        ((EntityLivingBase) source).setRevengeTarget(null);
+                        event.setCanceled(true);
+                        return;
+                    }
+                }
             }
-            
-            if (ConfigHandler.protectBabies && living instanceof EntityAgeable && ((EntityAgeable) living).isChild() && !source.isSneaking() && ConfigHandler.protectPets)
+
+            if (ConfigHandler.protectBabies && living instanceof EntityAgeable && ((EntityAgeable) living).isChild() && !source.isSneaking()) {
                 event.setCanceled(true);
+            }
         }
     }
 }
