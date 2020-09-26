6481f6472b418d8851fee1405c2c366823090e84
Tyler Hancock <darklime@live.ca>
2020-09-26 11:16:34 -0600

    Ported to 1.15.2

--- src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
+++ src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -18,62 +19,90 @@ public class FriendlyFire {
 	
     public FriendlyFire() {
 
+    	MinecraftForge.EVENT_BUS.addListener(this::onEntityAttack);
     	MinecraftForge.EVENT_BUS.addListener(this::onEntityHurt);
     	ModLoadingContext.get().registerConfig(Type.COMMON, configuration.getSpec());
     }
+    
+    private final void onEntityAttack (LivingAttackEvent event) {
+    	
+    	if (preventAttack(event.getEntityLiving(), event.getSource(), event.getAmount())) {
+    		
+    		event.setCanceled(true);
+    		event.getEntityLiving().setRevengeTarget(null);
+    		
+    		final Entity trueSource = event.getSource().getTrueSource();
+    		
+    		if (trueSource instanceof LivingEntity) {
+    			
+    			((LivingEntity) trueSource).setRevengeTarget(null);
+    		}
+    	}
+    }
+    
+    private final void onEntityHurt (LivingHurtEvent event) {
 
-    private void onEntityHurt (LivingHurtEvent event) {
+    	if (preventAttack(event.getEntityLiving(), event.getSource(), event.getAmount())) {
+    		
+    		event.setCanceled(true);
+    		event.setAmount(0f);
+    		event.getEntityLiving().setRevengeTarget(null);
+    		
+    		final Entity trueSource = event.getSource().getTrueSource();
+    		
+    		if (trueSource instanceof LivingEntity) {
+    			
+    			((LivingEntity) trueSource).setRevengeTarget(null);
+    		}
+    	}
+    }
+    
+    private final boolean preventAttack(Entity living, DamageSource source, float amount) {
+    	
+    	return source != null ? preventAttack(living, source.getTrueSource(), amount) : false;
+    }
+    
+    private final boolean preventAttack(Entity living, Entity source, float amount) {
+    	
+        // Check if the entity can be owned by the player
+        if (living instanceof TameableEntity) {
 
-        if (event.getEntityLiving() != null && event.getSource() != null && event.getSource().getTrueSource() != null) {
+        	// Checks if pets should be protected from players
+            if (configuration.shouldProtectPetsFromOwners()) {
 
-            final LivingEntity living = event.getEntityLiving();
-            final Entity source = event.getSource().getTrueSource();
-            
-            // Check if the entity can be owned by the player
-            if (living instanceof TameableEntity) {
-
-            	// Checks if pets should be protected from players
-                if (configuration.shouldProtectPetsFromOwners()) {
-
-                    final Entity owner = ((TameableEntity) living).getOwner();
+                final Entity owner = ((TameableEntity) living).getOwner();
+                
+                // Check if it is the owner dealing the damage, and the owner is not sneaking.
+                if (owner != null && owner.getUniqueID().equals(source.getUniqueID()) && !source.isShiftKeyDown()) {
                     
-                    // Check if it is the owner dealing the damage, and the owner is not sneaking.
-                    if (owner != null && owner.getUniqueID().equals(source.getUniqueID()) && !source.isSneaking()) {
-                        
-                    	// If reflection is set to true, the player will be damaged when attacking their pets.
-                    	if (configuration.shouldReflectDamage()) {
-                    		
-                    		owner.attackEntityFrom(DamageSource.GENERIC, event.getAmount());
-                    	}
-                    	
-                        event.setCanceled(true);
-                        event.setAmount(0);
-                        return;
-                    }
-                }
-
-                // Check if pets should be protected from pets with the same owner.
-                else if (configuration.shouldProtectPetsFromPets() && source instanceof TameableEntity) {
-
-                    final boolean sameOwner = ((TameableEntity) living).getOwnerId().equals(((TameableEntity) source).getOwnerId());
-
-                    if (sameOwner) {
-
-                        living.setRevengeTarget(null);
-                        ((LivingEntity) source).setRevengeTarget(null);
-                        event.setCanceled(true);
-                        event.setAmount(0);
-                        return;
-                    }
+                	// If reflection is set to true, the player will be damaged when attacking their pets.
+                	if (configuration.shouldReflectDamage()) {
+                		
+                		owner.attackEntityFrom(DamageSource.GENERIC, amount);
+                	}
+                	
+                	return true;
                 }
             }
 
-            // Check if child mobs can be killed.
-            if (configuration.shouldProtectChildren() && living instanceof AgeableEntity && ((AgeableEntity) living).isChild() && !source.isSneaking()) {
-            	
-                event.setCanceled(true);
-                event.setAmount(0);
+            // Check if pets should be protected from pets with the same owner.
+            else if (configuration.shouldProtectPetsFromPets() && source instanceof TameableEntity) {
+
+                final boolean sameOwner = ((TameableEntity) living).getOwnerId().equals(((TameableEntity) source).getOwnerId());
+
+                if (sameOwner) {
+
+                	return true;
+                }
             }
         }
+
+        // Check if child mobs can be killed.
+        if (configuration.shouldProtectChildren() && living instanceof AgeableEntity && ((AgeableEntity) living).isChild() && !source.isShiftKeyDown()) {
+
+        	return true;
+        }
+        
+    	return false;
     }
 }
