7c5b27a307deeeb915d416efb4097f03a26f9f9d
Tyler Hancock <darklime@live.ca>
2021-01-27 15:13:12 -0700

    Fix baby hoglins being immune. #13

--- src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
+++ src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
@@ -5,9 +5,9 @@ import org.apache.commons.lang3.tuple.Pair;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.passive.horse.AbstractHorseEntity;
-import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
@@ -118,6 +118,12 @@ public class FriendlyFire {
 
                 return true;
             }
+        }
+        
+        // Check if child mobs can be killed.
+        if (configuration.shouldProtectChildren() && !(living instanceof IMob) && living instanceof AgeableEntity && ((AgeableEntity) living).isChild() && !source.isSneaking()) {
+
+        	return true;
         }
         
     	return false;
