b5ce0b21d28d204d21480abc0da616ba9308be4d
Tyler Hancock <darklime@live.ca>
2021-03-08 23:48:51 -0700

    Added item tag to bypass pet damage protection, and to bypass all damage protection

--- src/main/java/net/darkhax/friendlyfire/Configuration.java
+++ src/main/java/net/darkhax/friendlyfire/Configuration.java
@@ -4,7 +4,7 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 
 public class Configuration {
-
+    
     private final ForgeConfigSpec spec;
     
     private final BooleanValue protectPetsFromOwner;
@@ -12,51 +12,51 @@ public class Configuration {
     private final BooleanValue protectChildren;
     private final BooleanValue reflectDamage;
     
-    public Configuration () {
-
+    public Configuration() {
+        
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-
+        
         // General Configs
         builder.comment("General settings for the mod.");
         builder.push("general");
-
+        
         builder.comment("Should pets be protected from damage dealt by their owners?");
-        protectPetsFromOwner = builder.define("protectPetsFromOwner", true);
+        this.protectPetsFromOwner = builder.define("protectPetsFromOwner", true);
         
         builder.comment("Should pets be protected from damage dealt by other pets with the same owner?");
-        protectPetsFromPets = builder.define("protectPetsFromOtherPets", true);
+        this.protectPetsFromPets = builder.define("protectPetsFromOtherPets", true);
         
         builder.comment("Should children mobs be protected from damage?");
-        protectChildren = builder.define("protectChildren", true);
+        this.protectChildren = builder.define("protectChildren", true);
         
         builder.comment("Should damage against friendly mobs be reflected back on the damage dealer?");
-        reflectDamage = builder.define("reflectDamage", false);
+        this.reflectDamage = builder.define("reflectDamage", false);
         
         this.spec = builder.build();
     }
     
-    public ForgeConfigSpec getSpec() {
-    	
-    	return this.spec;
+    public ForgeConfigSpec getSpec () {
+        
+        return this.spec;
     }
     
-    public boolean shouldProtectPetsFromOwners() {
-    	
-    	return protectPetsFromOwner.get();
-    }
-
-    public boolean shouldProtectPetsFromPets() {
-    	
-    	return protectPetsFromPets.get();
+    public boolean shouldProtectPetsFromOwners () {
+        
+        return this.protectPetsFromOwner.get();
     }
     
-    public boolean shouldProtectChildren() {
-    	
-    	return protectChildren.get();
+    public boolean shouldProtectPetsFromPets () {
+        
+        return this.protectPetsFromPets.get();
     }
     
-    public boolean shouldReflectDamage() {
-    	
-    	return reflectDamage.get();
+    public boolean shouldProtectChildren () {
+        
+        return this.protectChildren.get();
+    }
+    
+    public boolean shouldReflectDamage () {
+        
+        return this.reflectDamage.get();
     }
 }
--- src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
+++ src/main/java/net/darkhax/friendlyfire/FriendlyFire.java
@@ -1,5 +1,9 @@
 package net.darkhax.friendlyfire;
 
+import java.util.UUID;
+
+import javax.annotation.Nullable;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraft.entity.AgeableEntity;
@@ -8,6 +12,10 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.passive.horse.AbstractHorseEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tags.ITag.INamedTag;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
@@ -21,111 +29,121 @@ import net.minecraftforge.fml.network.FMLNetworkConstants;
 @Mod("friendlyfire")
 public class FriendlyFire {
     
-	private Configuration configuration = new Configuration();
-	
+    public static final INamedTag<Item> BYPASS_PET = ItemTags.makeWrapperTag("friendlyfire:bypass_pet");
+    public static final INamedTag<Item> BYPASS_ALL = ItemTags.makeWrapperTag("friendlyfire:bypass_all_protection");
+    
+    private final Configuration configuration = new Configuration();
+    
     public FriendlyFire() {
-
-    	ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
-    	MinecraftForge.EVENT_BUS.addListener(this::onEntityAttack);
-    	MinecraftForge.EVENT_BUS.addListener(this::onEntityHurt);
-    	ModLoadingContext.get().registerConfig(Type.COMMON, configuration.getSpec());
+        
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of( () -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        MinecraftForge.EVENT_BUS.addListener(this::onEntityAttack);
+        MinecraftForge.EVENT_BUS.addListener(this::onEntityHurt);
+        ModLoadingContext.get().registerConfig(Type.COMMON, this.configuration.getSpec());
     }
     
     private final void onEntityAttack (LivingAttackEvent event) {
-    	
-    	if (preventAttack(event.getEntityLiving(), event.getSource(), event.getAmount())) {
-    		
-    		event.setCanceled(true);
-    		event.getEntityLiving().setRevengeTarget(null);
-    		
-    		final Entity trueSource = event.getSource().getTrueSource();
-    		
-    		if (trueSource instanceof LivingEntity) {
-    			
-    			((LivingEntity) trueSource).setRevengeTarget(null);
-    		}
-    	}
+        
+        if (this.preventAttack(event.getEntityLiving(), event.getSource(), event.getAmount())) {
+            
+            event.setCanceled(true);
+            event.getEntityLiving().setRevengeTarget(null);
+            
+            final Entity trueSource = event.getSource().getTrueSource();
+            
+            if (trueSource instanceof LivingEntity) {
+                
+                ((LivingEntity) trueSource).setRevengeTarget(null);
+            }
+        }
     }
     
     private final void onEntityHurt (LivingHurtEvent event) {
-
-    	if (preventAttack(event.getEntityLiving(), event.getSource(), event.getAmount())) {
-    		
-    		event.setCanceled(true);
-    		event.setAmount(0f);
-    		event.getEntityLiving().setRevengeTarget(null);
-    		
-    		final Entity trueSource = event.getSource().getTrueSource();
-    		
-    		if (trueSource instanceof LivingEntity) {
-    			
-    			((LivingEntity) trueSource).setRevengeTarget(null);
-    		}
-    	}
-    }
-    
-    private final boolean preventAttack(Entity living, DamageSource source, float amount) {
-    	
-    	return source != null ? preventAttack(living, source.getTrueSource(), amount) : false;
-    }
-    
-    private final boolean preventAttack(Entity living, Entity source, float amount) {
-    	
-    	// filter out null exceptions.
-    	if (living == null || source == null) {
-    		
-    		return false;
-    	}
-    	
-        // Check if the entity can be owned by the player
-        if (living instanceof TameableEntity) {
-
-        	// Checks if pets should be protected from players
-            if (configuration.shouldProtectPetsFromOwners()) {
-
-                final Entity owner = ((TameableEntity) living).getOwner();
+        
+        if (this.preventAttack(event.getEntityLiving(), event.getSource(), event.getAmount())) {
+            
+            event.setCanceled(true);
+            event.setAmount(0f);
+            event.getEntityLiving().setRevengeTarget(null);
+            
+            final Entity trueSource = event.getSource().getTrueSource();
+            
+            if (trueSource instanceof LivingEntity) {
                 
-                // Check if it is the owner dealing the damage, and the owner is not sneaking.
-                if (owner != null && owner.getUniqueID().equals(source.getUniqueID()) && !source.isSneaking()) {
-                    
-                	// If reflection is set to true, the player will be damaged when attacking their pets.
-                	if (configuration.shouldReflectDamage()) {
-                		
-                		owner.attackEntityFrom(DamageSource.GENERIC, amount);
-                	}
-                	
-                	return true;
-                }
-            }
-
-            // Check if pets should be protected from pets with the same owner.
-            else if (configuration.shouldProtectPetsFromPets() && source instanceof TameableEntity) {
-
-                final boolean sameOwner = ((TameableEntity) living).getOwnerId().equals(((TameableEntity) source).getOwnerId());
-
-                if (sameOwner) {
-
-                	return true;
-                }
+                ((LivingEntity) trueSource).setRevengeTarget(null);
             }
         }
-
-        if (configuration.shouldProtectPetsFromPets() && living instanceof AbstractHorseEntity && !source.isSneaking()) {
-
-            final boolean sameOwner = source.getUniqueID().equals(((AbstractHorseEntity) living).getOwnerUniqueId());
-
-            if (sameOwner) {
-
+    }
+    
+    private final boolean preventAttack (Entity living, DamageSource source, float amount) {
+        
+        return source != null ? this.preventAttack(living, source.getTrueSource(), amount) : false;
+    }
+    
+    @Nullable
+    private UUID getOwner (Entity entity) {
+        
+        if (entity instanceof TameableEntity) {
+            
+            return ((TameableEntity) entity).getOwnerId();
+        }
+        
+        if (entity instanceof AbstractHorseEntity) {
+            
+            return ((AbstractHorseEntity) entity).getOwnerUniqueId();
+        }
+        
+        return null;
+    }
+    
+    private final boolean preventAttack (Entity living, Entity source, float amount) {
+        
+        // Null targets or sources can not be protected. Sneaking will bypass this mod
+        // entirely.
+        if (living == null || source == null || source.isSneaking()) {
+            
+            return false;
+        }
+        
+        // The item used by the attacker.
+        final ItemStack heldItem = source instanceof LivingEntity ? ((LivingEntity) source).getHeldItemMainhand() : ItemStack.EMPTY;
+        
+        // Items in the bypass all tag will always cause damage.
+        if (BYPASS_ALL.contains(heldItem.getItem())) {
+            
+            return false;
+        }
+        
+        // Gets the pet owner ID, will be null if not a pet mob.
+        final UUID ownerId = this.getOwner(living);
+        
+        if (ownerId != null && !BYPASS_PET.contains(heldItem.getItem())) {
+            
+            // Protects owners from hurting their pets.
+            if (this.configuration.shouldProtectPetsFromOwners() && ownerId.equals(source.getUniqueID())) {
+                
+                // Reflection causes players to hurt themselves instead.
+                if (this.configuration.shouldReflectDamage()) {
+                    
+                    source.attackEntityFrom(DamageSource.GENERIC, amount);
+                }
+                
+                return true;
+            }
+            
+            // Protect pets from pets with the same owner.
+            else if (this.configuration.shouldProtectPetsFromPets() && ownerId.equals(this.getOwner(source))) {
+                
                 return true;
             }
         }
         
         // Check if child mobs can be killed.
-        if (configuration.shouldProtectChildren() && !(living instanceof IMob) && living instanceof AgeableEntity && ((AgeableEntity) living).isChild() && !source.isSneaking()) {
-
-        	return true;
+        if (this.configuration.shouldProtectChildren() && !(living instanceof IMob) && living instanceof AgeableEntity && ((AgeableEntity) living).isChild() && !source.isSneaking()) {
+            
+            return true;
         }
         
-    	return false;
+        return false;
     }
 }
