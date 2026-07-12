4b16673804b28ee03178740c87c13e8fba9689df
Tyler Hancock <darklime@live.ca>
2026-07-11 22:53:27 -0600

    Fix edge cases where some mobs like Bees & Bats act like they were hit even if damage was prevented.

--- common/src/main/java/net/darkhax/friendlyfire/common/mixin/MixinLivingEntity.java
+++ common/src/main/java/net/darkhax/friendlyfire/common/mixin/MixinLivingEntity.java
@@ -4,20 +4,18 @@ import net.darkhax.friendlyfire.common.FriendlyFire;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.parrot.Parrot;
-import net.minecraft.world.entity.animal.wolf.Wolf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({LivingEntity.class, Wolf.class, Parrot.class})
+@Mixin({LivingEntity.class})
 public class MixinLivingEntity {
 
-    @Inject(method = "hurtServer", at = @At("HEAD"), cancellable = true)
-    private void onLivingHurt(ServerLevel level, DamageSource source, float damage, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "isInvulnerableTo", at = @At("HEAD"), cancellable = true)
+    private void onLivingHurt(ServerLevel level, DamageSource source, CallbackInfoReturnable<Boolean> cir) {
         if (FriendlyFire.preventAttack((LivingEntity) (Object) this, source)) {
-            cir.setReturnValue(false);
+            cir.setReturnValue(true);
         }
     }
 }
